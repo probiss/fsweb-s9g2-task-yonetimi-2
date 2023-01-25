6298a561bef37f38b9782c8bc25c9bbb6ed742f0
probiss <bulut.cihad@gmail.com>
2023-01-25 03:38:15 +0300

    date-fns ve çevresi düzenlendi...

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { formatDistanceToNow, differenceInDays } from 'date-fns';
+import { tr } from 'date-fns/locale';
 
 const Task = ({ taskObj, onComplete }) => {
-
+    const timeLeft = formatDistanceToNow(new Date(taskObj.deadline), {locale :tr });
+    //console.log(timeLeft);
+    const difference = differenceInDays(new Date(taskObj.deadline), new Date() );
+    const aga = new Date(taskObj.deadline) - new Date() < 0 ? "geçti":"kaldı" ;
+    const shine = difference <=3 ? true : false;
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline"> Son Teslim: <span className={difference <=3 ? "input-error": "" } >{timeLeft + " " + aga} </span> </div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
