0669bc01638b29eff60dd4c06951156276a3f046
probiss <bulut.cihad@gmail.com>
2023-01-25 05:26:18 +0300

    bir kısım tailwind denendi.

--- src/Task.js
+++ src/Task.js
@@ -9,16 +9,16 @@ const Task = ({ taskObj, onComplete }) => {
     const aga = new Date(taskObj.deadline) - new Date() < 0 ? "geçti":"kaldı" ;
     const shine = difference <=3 ? true : false;
   return (
-    <div className="task">
-      <h3>{taskObj.title}</h3>
-      <div className="deadline"> Son Teslim: <span className={difference <=3 ? "input-error": "" } >{timeLeft + " " + aga} </span> </div>
-      <p>{taskObj.description}</p>
+    <div className="p-6 bg-white rounded-md leading-normal mt-4 shadow-md">
+      <h3 className= "text-lg text-yellow-600" >{taskObj.title}</h3>
+      <div className="text-xs pt-1"> Son Teslim: <span className= {difference <=3 ? "text-xs pt-1 text-red-600 bg-red-100": "" } >{timeLeft + " " + aga} </span> </div>
+      <p className="px-2 py-3 text-sm text-gray-500" >{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className="inline-block py-1.5 px-3 text-sm border border-slate-300 border-solid mr-1 mb-2 rounded-3xl " key={p}>{p}</span>
         ))}
-      </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      </div >
+      {onComplete && <button className="block py-2 px-3 ml-auto bg-orange-200 shadow-md hover:shadow-lg rounded border-0 cursor-pointer" onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
     </div>
   );
 };
